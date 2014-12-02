1ce3ba3c39ac5afe0ff4b88e9a32faa8ac2d98af
Natali Ringel <natali.ringel@codeit.com.ua>
2014-12-02 18:02:04 +0200

    Module configs updates

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,75 +1,75 @@
 <?php
 
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
-                'type' => 'segment',
-								'options' => array(
-									'route' => '[/:controller[/:action]][/]',
-									'defaults' => array(
-										'__NAMESPACE__' => 'Application\Controller',
-										'controller' => 'Index',
-										'action' => 'index',
-									),
-									'constraints' => array(
-										'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-										'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-									),
-								),
-								'may_terminate' => true,
-            ),
-            'activeforgot' => array(
-							'type' => 'segment',
-							'options' => array(
-								'route' => '/user/activeforgot/:id/:code',
-								'defaults' => array(
-									'__NAMESPACE__' => 'Application\Controller',
-									'controller' => 'User',
-									'action' => 'activeforgot',
-								),
-								'constraints' => array(
-									'code' => '[a-zA-Z0-9_-]*',
-									'id' => '[0-9]*',
-								),
-							),
-						),
-        ),
-    ),
-    'controllers' => array(
-        'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController',
-            'Application\Controller\User' => 'Application\Controller\UserController',
-        ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => DEBUG,
-        'display_exceptions'       => DEBUG,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
+	'router' => array(
+		'routes' => array(
+			'home' => array(
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => array(
+					'route'    => '/',
+					'defaults' => array(
+						'controller' => 'Application\Controller\Index',
+						'action'     => 'index',
+					),
+				),
+			),
+			// The following is a route to simplify getting started creating
+			// new controllers and actions without needing to create a new
+			// module. Simply drop new controllers in, and you can access them
+			// using the path /application/:controller/:action
+			'application' => array(
+				'type' => 'segment',
+				'options' => array(
+					'route' => '[/:controller[/:action]][/]',
+					'defaults' => array(
+						'__NAMESPACE__' => 'Application\Controller',
+						'controller' => 'Index',
+						'action' => 'index',
+					),
+					'constraints' => array(
+						'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+						'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+					),
+				),
+				'may_terminate' => true,
+			),
+			'activeforgot' => array(
+				'type' => 'segment',
+				'options' => array(
+					'route' => '/user/activeforgot/:id/:code',
+					'defaults' => array(
+						'__NAMESPACE__' => 'Application\Controller',
+						'controller' => 'User',
+						'action' => 'activeforgot',
+					),
+					'constraints' => array(
+						'code' => '[a-zA-Z0-9_-]*',
+						'id' => '[0-9]*',
+					),
+				),
+			),
+		),
+	),
+	'controllers' => array(
+		'invokables' => array(
+			'Application\Controller\Index' => 'Application\Controller\IndexController',
+			'Application\Controller\User' => 'Application\Controller\UserController',
+		),
+	),
+	'view_manager' => array(
+		'display_not_found_reason' => DEBUG,
+		'display_exceptions'       => DEBUG,
+		'doctype'                  => 'HTML5',
+		'not_found_template'       => 'error/404',
+		'exception_template'       => 'error/index',
+		'template_map' => array(
+			'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+			'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+			'error/404'               => __DIR__ . '/../view/error/404.phtml',
+			'error/index'             => __DIR__ . '/../view/error/index.phtml',
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
 );
--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -2,151 +2,120 @@
 namespace Auth;
 
 return array(
-    'router' => array(
-        'routes' => array(
-            'auth' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/auth',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Auth\Controller',
-                        'controller' => 'Index',
-                        'action'     => 'login',
-                    ),
-                    'constraints' => array(
+	'router' => array(
+		'routes' => array(
+			'auth' => array(
+				'type'    => 'Literal',
+				'options' => array(
+					'route'    => '/auth',
+					'defaults' => array(
+						'__NAMESPACE__' => 'Auth\Controller',
+						'controller' => 'Index',
+						'action'     => 'login',
+					),
+					'constraints' => array(
 						'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
 						'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
 					),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                	'login' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/login[/]',
-                            'defaults' => array(
-                                'action'   => 'login',
-                            ),
-                        ),
-                    ),
-                    'registration' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/registration[/]',
-                            'defaults' => array(
-                                'action'   => 'registration',
-                            ),
-                        ),
-                    ),
-                    'forgotpassword' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/forgotpassword[/]',
-                            'defaults' => array(
-                                'action'   => 'forgotpassword',
-                            ),
-                        ),
-                    ),
-                    'google' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/google[/]',
-                            'defaults' => array(
-                                'action'   => 'socialNetworkLogin',
-                                'provider' => 'google',
-                            ),
-                        ),
-                    ),
-                    
-                    'facebook' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/facebook[/]',
-                            'defaults' => array(
-                                'action'   => 'socialNetworkLogin',
-                                'provider' => 'facebook',
-                            ),
-                        ),
-                    ),
-                    
-                    'logout' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/logout[/]',
-                            'defaults' => array(
-                                'action' => 'logout',
-                            ),
-                        ),
-                    ),
+				),
+				'may_terminate' => true,
+				'child_routes' => array(
+					'action' => array(
+						'type' => 'segment',
+						'options' => array(
+							'route' => '[/:action][/]',
+							'defaults' => array(
+								'__NAMESPACE__' => 'Auth\Controller',
+								'controller' => 'Index',
+								'action' => 'index',
+							),
+							'constraints' => array(
+								'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+							),
+						),
+						'may_terminate' => true,
+					),
+					'google' => array(
+						'type' => 'Segment',
+						'options' => array(
+							'route' => '/google[/]',
+							'defaults' => array(
+								'action'   => 'socialNetworkLogin',
+								'provider' => 'google',
+							),
+						),
+					),
+
+					'facebook' => array(
+						'type' => 'Segment',
+						'options' => array(
+							'route' => '/facebook[/]',
+							'defaults' => array(
+								'action'   => 'socialNetworkLogin',
+								'provider' => 'facebook',
+							),
+						),
+					),
 					'backend' => array(
-                        'type' => 'Literal',
-                        'options' => array(
-                            'route' => '/backend',
-                            'defaults' => array(
-                                'action' => 'backend',
-                            ),
-                        ),
-                    ),
-                    'activeforgot' => array(
-                        'type' => 'segment',
-                        'options' => array(
-                            'route' => '/activeforgot/:id/:code[/]',
-                            'defaults' => array(
-                                'action' => 'activeforgot',
-                            ),
-                            'constraints' => array(
-                                'code' => '[a-zA-Z0-9_-]*',
-                                'id' => '[0-9]*',
-                            ),
-                        ),
-                    ),
-                    'complete-registration' => array(
-                        'type' => 'segment',
-                        'options' => array(
-                            'route' => '/complete-registration[/]',
-                            'defaults' => array(
-                                'action' => 'completeRegistration',
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'controllers' => array(
-    	'factories' => array(
-            'Auth\Controller\Index' => 'Auth\Service\IndexControllerFactory',
-        ),
-    ),
-    'service_manager' => array(
-        'factories' => array(
-            'AuthBackend' => 'Auth\Service\HybridAuthFactory',
-            'AuthCurrentUser' => 'Auth\Service\UserFactory',
-        ),
-        'invokables' => array(
-            'Auth\Service\UserWrapperFactory' => 'Auth\Service\UserWrapperFactory',
-        ),
-    ),
-    'view_helpers' => array(
-        'invokables' => array(
-            'authinfo' => 'Auth\View\Helper\Auth',
-        ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => DEBUG,
-        'display_exceptions' => DEBUG,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'Auth/error/404',
-        'exception_template' => 'Auth/error/index',
-        'template_map' => array(
-            'Auth/error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'Auth/error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-        'strategies' => array(
-            'ViewJsonStrategy',
-        ),
-    ),
+						'type' => 'Literal',
+						'options' => array(
+							'route' => '/backend',
+							'defaults' => array(
+								'action' => 'backend',
+							),
+						),
+					),
+					'activeforgot' => array(
+						'type' => 'segment',
+						'options' => array(
+							'route' => '/activeforgot/:id/:code[/]',
+							'defaults' => array(
+								'action' => 'activeforgot',
+							),
+							'constraints' => array(
+								'code' => '[a-zA-Z0-9_-]*',
+								'id' => '[0-9]*',
+							),
+						),
+					),
+				),
+			),
+		),
+	),
+	'controllers' => array(
+		'factories' => array(
+			'Auth\Controller\Index' => 'Auth\Service\IndexControllerFactory',
+		),
+	),
+	'service_manager' => array(
+		'factories' => array(
+			'AuthBackend' => 'Auth\Service\HybridAuthFactory',
+			'AuthCurrentUser' => 'Auth\Service\UserFactory',
+		),
+		'invokables' => array(
+			'Auth\Service\UserWrapperFactory' => 'Auth\Service\UserWrapperFactory',
+		),
+	),
+	'view_helpers' => array(
+		'invokables' => array(
+			'authinfo' => 'Auth\View\Helper\Auth',
+		),
+	),
+	'view_manager' => array(
+		'display_not_found_reason' => DEBUG,
+		'display_exceptions' => DEBUG,
+		'doctype' => 'HTML5',
+		'not_found_template' => 'Auth/error/404',
+		'exception_template' => 'Auth/error/index',
+		'template_map' => array(
+			'Auth/error/404'               => __DIR__ . '/../view/error/404.phtml',
+			'Auth/error/index'             => __DIR__ . '/../view/error/index.phtml',
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+		'strategies' => array(
+			'ViewJsonStrategy',
+		),
+	),
 );
