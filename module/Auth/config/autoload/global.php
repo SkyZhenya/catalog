8841163e138fd00bf324eab7c6cff818cd69081f
Natali Ringel <natali.ringel@codeit.com.ua>
2014-12-03 12:44:27 +0200

    Auth module config

---
+++ module/Auth/config/autoload/global.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+	'Auth' => array(
+		'backend' => 'Google',
+		'hybrid_auth' => array(
+			'providers' => array(
+				"Google" => array (
+					"enabled" => true,
+					"keys"    => array ( "id" => GA_APP_ID, "secret" => GA_SECRET ),
+					"scope"           => "https://www.googleapis.com/auth/userinfo.profile ". // optional
+					"https://www.googleapis.com/auth/userinfo.email"   , // optional
+					"access_type"     => "offline",   // optional
+					"approval_prompt" => "force",     // optional
+				),
+				"Facebook" => array (
+					"enabled" => true,
+					"keys"    => array ( "id" => FACEBOOK_APP_ID, "secret" => FACEBOOK_SECRET ),
+					"scope"   => "email, user_about_me, user_birthday, user_hometown", // optional
+					"display" => "popup" // optional
+				)
+			),
+			'debug_mode' => false,
+			// For some reason Hybrid_Auth doesn't create file in a specified folder, be sure that it exists
+			'debug_file' => __DIR__ . '/hybrid_auth.log',
+		),
+		'session_name' => 'hybridauth',
+	)
+);
