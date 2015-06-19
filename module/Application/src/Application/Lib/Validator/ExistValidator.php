a078bb29bdc82948304eb539511b9aff73d78a29
Natali Ringel <natali.ringel@codeit.com.ua>
2015-06-19 14:43:13 +0300

    Added two new validators

---
+++ module/Application/src/Application/Lib/Validator/ExistValidator.php
@@ -0,0 +1,44 @@
+<?php
+namespace Application\Lib\Validator;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Db\Sql\Expression;
+
+class ExistValidator extends AbstractValidator{
+
+	/**
+	 * @var \Application\Model\AppTable
+	 */
+	private $model;
+	private $field;
+
+	const NOTEXIST = 'aexist';
+
+	protected $messageTemplates = array(
+		self::NOTEXIST => 'aexist',
+	);
+
+	/**
+	 * @param \Application\Model\AppTable $model
+	 * @param string $field
+	 * @param string $message
+	 */
+	public function __construct(\Application\Model\AppTable $model, $field, $message = 'Item is not found') {
+		parent::__construct();
+		$this->messageTemplates = array(
+			self::NOTEXIST => $message,
+		);
+		$this->setMessage($message);
+		$this->model = $model;
+		$this->field = $field;
+	}
+
+	public function isValid($value) {
+		if($this->model->select([$this->field => $value])->count()) {
+			return true;
+		}
+		
+		$this->error(self::NOTEXIST);
+		return false;
+	}
+}
