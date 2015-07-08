5d70c377a440a03c71f65fd2483dce4e906535e8
Natali Ringel <natali.ringel@codeit.com.ua>
2015-07-08 17:49:34 +0300

    New Tools module has been added

---
+++ module/Tools/src/Tools/Controller/DevtoolsController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tools\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+/**
+ * different tools for developing, populationg DB etc.
+ */
+class DevtoolsController extends AbstractActionController {
+	
+
+}
